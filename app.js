7d19da6511bc8b45c020f1fa428bcc20c640c776
Amanat Khan Shakil <amanatkhanshakil223@gmail.com>
2024-09-23 02:53:22 +0600

    work in progress at bus select form

--- app.js
+++ app.js
@@ -2,28 +2,47 @@ const seatList = document.getElementById("seat-list");
 const totalSeat = document.getElementById("total-seats");
 const noSeat = document.getElementById("no-seat");
 const avaiableSeat = document.getElementById("avaiable-seats");
+const totalPrice = document.getElementById("total-price");
+const cuponInput = document.getElementById("cupon-input");
+const cuponBtn = document.getElementById("cupon-btn");
 
 let selectedSeat = [];
 
 function handleSelectSeat(event) {
-  event.classList.add("bg-primary", "text-white");
+  const value = event.innerText;
 
-  selectedSeat.push(event.innerText);
-  totalSeat.innerText = selectedSeat.length;
+  if (selectedSeat.includes(value)) {
+    return alert("Seat Already Booked");
+  } else if (selectedSeat.length < 4) {
+    event.classList.add("bg-primary", "text-white");
 
-  avaiableSeat.innerText = parseInt(avaiableSeat.innerText) - 1;
+    selectedSeat.push(event.innerText);
+    totalSeat.innerText = selectedSeat.length;
 
-  if (selectedSeat.length > 0) {
-    noSeat.classList.add("hidden");
-  }
+    avaiableSeat.innerText = parseInt(avaiableSeat.innerText) - 1;
 
-  seatList.innerHTML += `
-                 <li
-                  class="flex justify-between items-center text-base text-[#030712]/70 my-3"
-                >
-                  <p>${event.innerText}</p>
-                  <p>Economy</p>
-                  <p>550</p>
-                </li>
+    if (selectedSeat.length > 0) {
+      noSeat.classList.add("hidden");
+    }
+
+    seatList.innerHTML += `
+  <li
+  class="flex justify-between items-center text-base text-[#030712]/70 my-3"
+  >
+  <p>${event.innerText}</p>
+  <p>Economy</p>
+  <p>550</p>
+  </li>
   `;
+
+    let total = selectedSeat.length * 550;
+    totalPrice.innerText = total.toFixed(2);
+
+    if (selectedSeat.length > 3) {
+      cuponInput.removeAttribute("disabled");
+      cuponBtn.removeAttribute("disabled");
+    }
+  } else {
+    return alert("Maximun Seat is Booked");
+  }
 }
